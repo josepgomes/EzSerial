4de40b7165c3220216a8ed6d26ad566adedee491
José Gomes <josepgomes@ua.pt>
2017-11-27 17:42:32 +0000

    Update ezserial.h

--- ezserial.h
+++ ezserial.h
@@ -11,9 +11,6 @@ public:
 
   EzSerial(std::string deviceName, int baud);
   ~EzSerial();
-
-  bool Send( unsigned char  * data,int len);
-  bool Send(unsigned char value);
   bool Send( std::string value);
   std::string Receive();
   bool IsOpen(void);
